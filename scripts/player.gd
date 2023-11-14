44ee68a82605cc9cdc76318eab7a9cd1cb91241b
michellegramolini <michelle.gramolini@gmail.com>
2023-11-14 15:54:14 -0500

    input actions; controller support; keyboard support

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,21 +17,22 @@ onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * j
 onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 onready var jump_timer: Timer = $CoyoteTimer
 
+# TODO: input actions
 
 func _physics_process(delta):
 	velocity.y += get_gravity() * delta
 
 	# Applying acceleration and friction
 	var target_velocity = get_input_velocity() * speed
-    #  For smooth speed manipulation, use the interpolate function
+	#  For smooth speed manipulation, use the interpolate function
 	velocity.x = interpolate(velocity.x, target_velocity, acceleration * delta)
 
 	var was_on_floor = is_on_floor()
 
-    # Jumping
-	if Input.is_action_just_pressed("ui_up"):
+	# Jumping
+	if Input.is_action_just_pressed("jump"):
 		jump()
-	if Input.is_action_just_released("ui_up"):
+	if Input.is_action_just_released("jump"):
 		cancel_jump()
 
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -46,30 +47,30 @@ func get_gravity() -> float:
 
 func get_input_velocity() -> float:
 	var horizontal_velocity := 0.0
-    # Horizontal movement
-	if Input.is_action_pressed("ui_right"):
+	# Horizontal movement
+	if Input.is_action_pressed("move_right"):
 		horizontal_velocity += 1.0
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		horizontal_velocity -= 1.0
 	
 	return horizontal_velocity
 
 func jump():
-    """When Jump action is pressed, Jump if the player is on the floor or within the coyote time."""
-    if is_on_floor() or !jump_timer.is_stopped():
-	    velocity.y = jump_velocity
+	"""When Jump action is pressed, Jump if the player is on the floor or within the coyote time."""
+	if is_on_floor() or !jump_timer.is_stopped():
+		velocity.y = jump_velocity
 
 func cancel_jump():
-    """Cancel the jump if the player releases the jump button early."""
-    if velocity.y < variable_jump_velocity_floor:
-        velocity.y = variable_jump_velocity_floor
+	"""Cancel the jump if the player releases the jump button early."""
+	if velocity.y < variable_jump_velocity_floor:
+		velocity.y = variable_jump_velocity_floor
 
 func interpolate(current: float, target: float, delta: float) -> float:
-    """Interpolate between current and target values by delta"""
-    if current < target:
-        return min(current + delta, target)
-    elif current > target:
-        return max(current - delta, target)
-    else:
-        return current
+	"""Interpolate between current and target values by delta"""
+	if current < target:
+		return min(current + delta, target)
+	elif current > target:
+		return max(current - delta, target)
+	else:
+		return current
 
