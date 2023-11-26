63b965659d07bdd9799ac1a84d89edc978c04c74
michellegramolini <michelle.gramolini@gmail.com>
2023-11-26 11:56:44 -0500

    Title screen music and effects

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var level_music = get_node("Music")
 onready var victory_music = get_node("Victory")
+onready var title_music = get_node("Title")
+onready var start_effect = get_node("Start")
 onready var level_starter = get_tree().get_nodes_in_group("Start")
 
 # Called when the node enters the scene tree for the first time.
@@ -9,6 +11,15 @@ func _ready():
 	# Connect to the Start node to start the timer
 	if level_starter != null and len(level_starter) > 0:
 		level_starter[0].connect("start_level", self, "_on_Start_start_level")
+
+func play_title_music():
+	title_music.play()
+
+func stop_title_music():
+	title_music.stop()
+
+func play_start_effect():
+	start_effect.play()
 
 # Signals
 func _on_Start_start_level():
